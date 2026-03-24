ff8cf12b17ddf9bbc1ebfee40a064d114ba98696
manishyadav02 <raomanishyadav18@gmail.com>
2026-03-24 14:50:39 +0530

    Fix nodemailer secure connection

--- services/emailService.js
+++ services/emailService.js
@@ -9,6 +9,7 @@ export const sendEmail = async (options) => {
     const transporter = nodeMailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        secure: true,
         auth: { 
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
